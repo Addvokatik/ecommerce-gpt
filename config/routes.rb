df8d33f18f2c1dd858ac561871278eb55be19c29
bodji_git <addvockat@gmail.com>
2024-02-19 19:53:17 +0200

    products

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,18 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
+  resources :products, only: [:index, :show]
+  root 'products#index'
+
   get '/home', to: 'home#index', as: 'home'
   get '/about', to: 'home#about', as: 'about'
   get '/contact', to: 'home#contact', as: 'contact'
   get 'signup', to: 'registrations#new', as: 'signup'
   post 'signup', to: 'registrations#create'
-  # ...
+
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions',
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
-
-  # devise_for :admin_users, controllers: {
-    # registrations: 'admin_users/registrations',
-    # sessions: 'admin_users/sessions'
-    # omniauth_callbacks: 'admin_users/omniauth_callbacks'
-  # }
 
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
@@ -28,4 +24,5 @@ Rails.application.routes.draw do
 
   # Інші маршрути...
   # ...
+ 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_01_141619) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_19_132946) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -34,6 +34,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_01_141619) do
     t.datetime "updated_at", null: false
     t.string "email"
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
